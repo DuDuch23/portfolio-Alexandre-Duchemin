491fa90f3f6c12981396aba362d0e34097d7b5c1
DuDuch23 <alexduduch77@gmail.com>
2023-04-29 23:05:03 +0200

    Update connexion_bdd.php

--- portfolio/require/connexion_bdd.php
+++ portfolio/require/connexion_bdd.php
@@ -1,17 +1,15 @@
 <?php
-
-$user = "116313_aduchemin";
-$pass = "Mushu4monchat?";
-$dbName = "lyceestvincent_aduchemin";
+$user = "root";
+$pass = "";
+$dbName = "portfolio";
 
 try{
-    $con = new \PDO("mysql:host=mysql-lyceestvincent.alwaysdata.net;dbname=$dbName;charset=UTF8", $user, $pass);
-    $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $con = new \PDO("mysql:host=127.0.0.1;dbname=$dbName;charset=UTF8", $user, $pass);
+    $con->setAttribute(pdo::ATTR_ERRMODE, pdo::ERRMODE_EXCEPTION);
 }
 catch (PDOException $exception)
 {
     echo 'Erreur lors de la connexion à la base de données : ' . $exception->getMessage();
     exit;
 }
-
 ?>
